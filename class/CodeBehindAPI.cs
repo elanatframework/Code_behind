8aeeb6d91d481e799bb47b5dde69371fd243cca4
Elanat Framework <111444759+elanatframework@users.noreply.github.com>
2023-11-06 00:29:20 +0330

    Add files via upload

---
+++ class/CodeBehindAPI.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+
+namespace CodeBehind.API
+{
+    public class Path
+    {
+        public string RunTimePath { get; private set; }
+        public string AspRunTimePath { get; private set; }
+        public string BaseDirectory { get; private set; }
+
+        public Path()
+        {
+            BaseDirectory = AppContext.BaseDirectory;
+            BaseDirectory = BaseDirectory.Replace("\\", "/");
+            RunTimePath = System.IO.Path.GetDirectoryName(typeof(object).GetTypeInfo().Assembly.Location);
+            RunTimePath = RunTimePath.Replace("\\", "/");
+            string SharedPath = RunTimePath.GetTextBeforeLastValue("/").GetTextBeforeLastValue("/");
+            string Version = RunTimePath.GetTextAfterLastValue("/");
+            AspRunTimePath = SharedPath + "/Microsoft.AspNetCore.App/" + Version; 
+        }
+    }
+}
