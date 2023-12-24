effcaaf005e82bc66c39118350146debeab20f6c
Elanat Framework <111444759+elanatframework@users.noreply.github.com>
2023-12-24 15:36:43 +0330

    Update CodeBehindModel.cs

--- class/CodeBehindModel.cs
+++ class/CodeBehindModel.cs
@@ -1,12 +1,19 @@
-﻿namespace CodeBehind
+namespace CodeBehind
 {
     public abstract class CodeBehindModel
     {
         public string ResponseText = "";
+        public HtmlData.NameValueCollection ViewData = new HtmlData.NameValueCollection();
+        public string DownloadFilePath { get; protected set; } = "";
 
         public void Write(string Text)
         {
             ResponseText += Text;
+        }
+
+        public void Download(string FilePath)
+        {
+            DownloadFilePath = FilePath;
         }
     }
 }
