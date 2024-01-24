b20cbca767a37d33937e0da8278c8d075b8c0dc3
Elanat Framework <111444759+elanatframework@users.noreply.github.com>
2024-01-24 14:26:07 +0330

    Update CodeBehindController.cs

--- class/CodeBehindController.cs
+++ class/CodeBehindController.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Http;
+using System.Reflection;
+
 namespace CodeBehind
 {
     public abstract class CodeBehindController
@@ -6,8 +9,23 @@ namespace CodeBehind
         public string ResponseText = "";
         public bool IgnoreViewAndModel = false;
         public HtmlData.NameValueCollection ViewData = new HtmlData.NameValueCollection();
+        public ValueCollectionLock Section = new ValueCollectionLock();
         public string ViewPath { get; protected set; } = "";
         public string DownloadFilePath { get; protected set; } = "";
+
+        /// <summary>
+        /// This Attribute Does Not Have A Value In The Constructor Method Of The Class, And Is Set Only After An Instance Of The Class Is Created.
+        /// </summary>
+        public string CallerViewPath { get; set; } = "";
+        /// <summary>
+        /// This Attribute Does Not Have A Value In The Constructor Method Of The Class, And Is Set Only After An Instance Of The Class Is Created.
+        /// </summary>
+        public string CallerViewDirectoryPath { get; set; } = "";
+
+        public void PageLoad(HttpContext context)
+        {
+
+        }
 
         public void Write(string Text)
         {
