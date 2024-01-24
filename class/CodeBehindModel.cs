f1c8a179fc35893755996abb9f9a72a64fe79bc4
Elanat Framework <111444759+elanatframework@users.noreply.github.com>
2024-01-24 14:29:50 +0330

    Update CodeBehindModel.cs

--- class/CodeBehindModel.cs
+++ class/CodeBehindModel.cs
@@ -4,7 +4,16 @@ namespace CodeBehind
     {
         public string ResponseText = "";
         public HtmlData.NameValueCollection ViewData = new HtmlData.NameValueCollection();
+        public ValueCollectionLock Section = new ValueCollectionLock();
         public string DownloadFilePath { get; protected set; } = "";
+        /// <summary>
+        /// This Attribute Does Not Have A Value In The Constructor Method Of The Class, And Is Set Only After An Instance Of The Class Is Created.
+        /// </summary>
+        public string CallerViewPath { get; set; } = "";
+        /// <summary>
+        /// This Attribute Does Not Have A Value In The Constructor Method Of The Class, And Is Set Only After An Instance Of The Class Is Created.
+        /// </summary>
+        public string CallerViewDirectoryPath { get; set; } = "";
 
         public void Write(string Text)
         {
