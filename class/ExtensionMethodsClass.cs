a8c89aa36cb82bcb734b3433205524c83f78402d
Elanat Framework <111444759+elanatframework@users.noreply.github.com>
2024-01-24 14:30:49 +0330

    Update ExtensionMethodsClass.cs

--- class/ExtensionMethodsClass.cs
+++ class/ExtensionMethodsClass.cs
@@ -48,6 +48,37 @@ namespace CodeBehind
             return Text.Substring(0, Text.LastIndexOf(Value));
         }
 
+        public static int ToNumber(this string Text)
+        {
+            if (string.IsNullOrEmpty(Text))
+                return 0;
+
+            int i;
+
+            int.TryParse(Text, out i);
+
+            return i;
+        }
+
+        public static int ToNumber(this object Text)
+        {
+            return ToNumber(Text.ToString());
+        }
+
+        public static bool IsNumber(this string Text)
+        {
+            for (int i = 0; i < Text.Length; i++)
+                if (!char.IsNumber(Text[i]))
+                    return false;
+
+            return true;
+        }
+
+        public static bool IsNumber(this object Text)
+        {
+            return IsNumber(Text.ToString());
+        }
+
         public static string ToMethodNameClean(this string Text)
         {
             Regex regex = new Regex("[^a-zA-Z0-9_]");
