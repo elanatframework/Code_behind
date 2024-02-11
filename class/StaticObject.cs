26d78693ef74dbda7efdcd2cb58f8e024d3131d2
Elanat Framework <111444759+elanatframework@users.noreply.github.com>
2024-02-11 19:30:12 +0330

    Add files via upload

---
+++ class/StaticObject.cs
@@ -0,0 +1,19 @@
+﻿namespace CodeBehind
+{
+    public static class StaticObject
+    {
+        private static bool StaticObjectHasInitialization { get; set; } = false;
+        public static bool PreventAccessDefaultAaspx { get; private set; } = false;
+
+        public static void SetValue()
+        {
+            if (StaticObjectHasInitialization)
+                return;
+
+            CodeBehindOptions options = new CodeBehindOptions();
+            PreventAccessDefaultAaspx = options.PreventAccessDefaultAaspx;
+
+            StaticObjectHasInitialization = true;
+        }
+    }
+}
