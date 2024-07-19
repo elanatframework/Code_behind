a9f989d479d18f7d3cea6b8fc8aebd7c13f08e02
Elanat Framework <111444759+elanatframework@users.noreply.github.com>
2024-07-20 00:12:04 +0330

    Update StaticObject.cs

--- class/StaticObject.cs
+++ class/StaticObject.cs
@@ -4,7 +4,9 @@ namespace CodeBehind
     {
         private static bool StaticObjectHasInitialization { get; set; } = false;
         public static bool PreventAccessDefaultAspx { get; private set; } = false;
+        public static string ViewPath { get; private set; }
         public static string DefaultRole { get; private set; }
+        public static string ViewPlace { get; private set; }
 
         public static void SetValue()
         {
@@ -13,7 +15,9 @@ namespace CodeBehind
 
             CodeBehindOptions options = new CodeBehindOptions();
             PreventAccessDefaultAspx = options.PreventAccessDefaultAspx;
+            ViewPath = options.ViewPath;
             DefaultRole = options.DefaultRole;
+            ViewPlace = options.WebFormsViewPlace;
 
             StaticObjectHasInitialization = true;
         }
