b989653da7d3e8d05e8a573762f2111a9df8f796
Elanat Framework <111444759+elanatframework@users.noreply.github.com>
2024-09-09 16:57:44 +0330

    Add files via upload

---
+++ class/RequestQuery.cs
@@ -0,0 +1,49 @@
+﻿using CodeBehind.HtmlData;
+using Microsoft.AspNetCore.Http;
+
+namespace CodeBehind
+{
+    internal class RequestQuery
+    {
+        public void AddQueryString(HttpContext context, string QueryString)
+        {
+            if (string.IsNullOrEmpty(QueryString))
+                return;
+
+            NameCollection QueryValues = new NameCollection();
+            QueryString TmpQueryString = new QueryString();
+            string[] QueryElements = QueryString.Split('&');
+            foreach (string element in QueryElements)
+            {
+                string[] NameValue = element.Split('=');
+
+                if (NameValue.Length > 1)
+                    TmpQueryString = TmpQueryString.Add(NameValue[0], NameValue[1]);
+                else
+                    TmpQueryString = TmpQueryString.Add(NameValue[0], "");
+
+                QueryValues.Add(NameValue[0]);
+            }
+
+            string RequestQueryString = context.Request.QueryString.Value;
+
+            if (!string.IsNullOrEmpty(RequestQueryString))
+            {
+                RequestQueryString = RequestQueryString.GetTextAfterValue("?");
+                string[] TmpQueryElements = RequestQueryString.Split('&');
+                foreach (string element in TmpQueryElements)
+                {
+                    string[] NameValue = element.Split('=');
+
+                    if (!QueryValues.Exist(NameValue[0]))
+                        if (NameValue.Length > 1)
+                            TmpQueryString = TmpQueryString.Add(NameValue[0], NameValue[1]);
+                        else
+                            TmpQueryString = TmpQueryString.Add(NameValue[0], "");
+                }
+            }
+
+            context.Request.QueryString = TmpQueryString;
+        }
+    }
+}
