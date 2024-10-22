f79cc2407365b3fba2488189eab5d3e92576f90d
Elanat Framework <111444759+elanatframework@users.noreply.github.com>
2024-10-22 16:02:59 +0330

    Update DefaultPages.cs

--- class/DefaultPages.cs
+++ class/DefaultPages.cs
@@ -399,9 +399,12 @@ function PostBack(obj, ViewState)
             var IsWebForms = false;
 
             // Check Exist WebForms Values
-            if (HttpResult.length >= 11)
-                if (HttpResult.substring(0, 11) == ""[web-forms]"")
+            if (HttpResult.TrimStart().length >= 11)
+                if (HttpResult.TrimStart().substring(0, 11) == ""[web-forms]"")
+                {
+                    HttpResult = HttpResult.TrimStart();
                     IsWebForms = true;
+                }
 
             if (IsWebForms)
                 cb_SetWebFormsValues(RequestName, HttpResult, true);
@@ -566,9 +569,12 @@ function GetBack(FormAction, ViewState)
             var IsWebForms = false;
 
             // Check Exist WebForms Values
-            if (HttpResult.length >= 11)
-                if (HttpResult.substring(0, 11) == ""[web-forms]"")
+            if (HttpResult.TrimStart().length >= 11)
+                if (HttpResult.TrimStart().substring(0, 11) == ""[web-forms]"")
+                {
+                    HttpResult = HttpResult.TrimStart();
                     IsWebForms = true;
+                }
 
             if (IsWebForms)
                 cb_SetWebFormsValues(RequestName, HttpResult, true);
@@ -1849,6 +1855,11 @@ String.prototype.toDOM = function ()
 String.prototype.FullTrim = function ()
 {
     return this.trim().replace(/^\s\n+|\s\n+$/g, '');
+};
+
+String.prototype.TrimStart = function ()
+{
+    return this.replace(/^[\s\n]+/, '');
 };
 
 String.prototype.IsInput = function ()
