c77dba4fca34d3e35d9c6a93dd55468627db5c6f
Elanat Framework <111444759+elanatframework@users.noreply.github.com>
2026-08-15 15:58:04 +0330

    Add files via upload

---
+++ class/ExceptionLog.cs
@@ -0,0 +1,11 @@
+﻿namespace CodeBehind
+{
+    public class ExceptionLog
+    {
+        public ExceptionLog(Exception ex)
+        {
+            Console.WriteLine("CodeBehind Error:");
+            Console.WriteLine(ex);
+        }
+    }
+}
